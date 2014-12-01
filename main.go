1a0b735810ef17c2df074f607191f5e5a4c1f66f
pocke <p.ck.t22@gmail.com>
2014-12-02 08:41:04 +0900

    add TODOs

--- main.go
+++ main.go
@@ -10,10 +10,13 @@ type server string
 
 // action-icons, actions, body, body-hyperlinks, body-images, body-markup, icon-multi, icon-static, persistence, sound
 func (s server) GetCapabilities() ([]string, *dbus.Error) {
+	// TODO: select return variables
 	return []string{"body"}, nil
 }
 
 func (s server) Notify(appName string, replacesID uint32, appIcon, summary, body string, actions []string, hints map[string]dbus.Variant, expireTimeout int32) (uint32, *dbus.Error) {
+	// TODO: result
+	// TODO: Do notify
 	fmt.Println(summary)
 	fmt.Println(body)
 	return 1, nil
@@ -27,6 +30,7 @@ func (s server) CloseNotification(id uint32) *dbus.Error {
 }
 
 func (s server) GetServerInformation() (name, vendor, version, specVersion string, err *dbus.Error) {
+	// TODO: Hard coded result
 	return "GoNotify", "pocke", "0.0.1", "1.2", nil
 }
 
