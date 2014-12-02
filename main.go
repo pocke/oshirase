ee7610c94af84d9106d070a002c0a5d0f7055278
pocke <p.ck.t22@gmail.com>
2014-12-02 09:43:25 +0900

    structize

--- main.go
+++ main.go
@@ -6,15 +6,35 @@ import (
 	"github.com/godbus/dbus"
 )
 
-type server string
+type Server struct {
+	conn *dbus.Conn
+}
+
+func NewServer() (*Server, error) {
+	conn, err := dbus.SessionBus()
+	if err != nil {
+		return nil, err
+	}
+	// XXX: reply処理
+	_, err = conn.RequestName("org.freedesktop.Notifications", dbus.NameFlagDoNotQueue)
+	if err != nil {
+		return nil, err
+	}
+
+	s := &Server{conn}
+
+	conn.Export(s, "/org/freedesktop/Notifications", "org.freedesktop.Notifications")
+
+	return s, nil
+}
 
 // action-icons, actions, body, body-hyperlinks, body-images, body-markup, icon-multi, icon-static, persistence, sound
-func (s server) GetCapabilities() ([]string, *dbus.Error) {
+func (s Server) GetCapabilities() ([]string, *dbus.Error) {
 	// TODO: select return variables
 	return []string{"body"}, nil
 }
 
-func (s server) Notify(appName string, replacesID uint32, appIcon, summary, body string, actions []string, hints map[string]dbus.Variant, expireTimeout int32) (uint32, *dbus.Error) {
+func (s Server) Notify(appName string, replacesID uint32, appIcon, summary, body string, actions []string, hints map[string]dbus.Variant, expireTimeout int32) (uint32, *dbus.Error) {
 	// TODO: result
 	// TODO: Do notify
 	fmt.Println(summary)
@@ -22,32 +42,23 @@ func (s server) Notify(appName string, replacesID uint32, appIcon, summary, body
 	return 1, nil
 }
 
-func (s server) CloseNotification(id uint32) *dbus.Error {
+func (s Server) CloseNotification(id uint32) *dbus.Error {
 	// TODO: delete Notification
 	//       Emitte NotificationClosed signal.
 	//       if notification dosen't exists, return empty dbus-error
 	return nil
 }
 
-func (s server) GetServerInformation() (name, vendor, version, specVersion string, err *dbus.Error) {
+func (s Server) GetServerInformation() (name, vendor, version, specVersion string, err *dbus.Error) {
 	// TODO: Hard coded result
 	return "GoNotify", "pocke", "0.0.1", "1.2", nil
 }
 
 func main() {
-	conn, err := dbus.SessionBus()
+	_, err := NewServer()
 	if err != nil {
 		panic(err)
 	}
-
-	// XXX: reply処理
-	_, err = conn.RequestName("org.freedesktop.Notifications", dbus.NameFlagDoNotQueue)
-	if err != nil {
-		panic(err)
-	}
-
-	n := server("Notify!!")
-	conn.Export(n, "/org/freedesktop/Notifications", "org.freedesktop.Notifications")
 
 	fmt.Println("start")
 
